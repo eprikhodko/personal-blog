16105e372a9058a6939cbdb0e18f6d423eed77ed
eprikhodko <eprikhodkodev@gmail.com>
2022-01-03 18:07:56 +0300

    Add styled-components package and create global style for project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "personal-blog",
+    siteUrl: "https://www.evgenyprikhodko.com",
+    title: "personal blog of Evgeny Prikhodko",
   },
-  plugins: [],
-};
+  plugins: [`gatsby-plugin-styled-components`],
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,40 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+
+html, body, #root, .App {
+  height: 100%;
+}
+
+/* set base font size to 10px */
+html {
+  /* 62.5% of 16px base font size is 10px */
+  font-size: 62.5%;
+}
+
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  /* font-family: 'Inter', sans-serif; */
+  /* if we won't specify font-size of the body, font size of html will be used instead, which is 16px in this case.
+  This way we can use 1em as 10px. If we'll specify the font-size of body and set it to 1.6rem, (which is 16px),
+  1em will become 16px */
+  font-size: 1.6rem;
+  color: rgba(0, 0, 0, 0.7);
+  font-weight: 500;
+  line-height: 1.6;
+  /* background-color: #dbdbdb; */
+}
+`
+
 function Layout({ pageTitle, children }) {
   return (
     <div>
+      <GlobalStyle />
       <title>{pageTitle}</title>
       <nav>
         <ul>
