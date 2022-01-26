a4d278c15a8d8d0cd21916bbfffac2aa46e7dc0a
eprikhodko <eprikhodkodev@gmail.com>
2022-01-26 10:56:15 +0300

    Show blog post date updated only if it is provided by frontmatter

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -9,14 +9,26 @@ import Layout from "../../components/Layout"
 const Date = styled.p`
   font-size: 1.9rem;
   font-weight: 500;
+  margin-bottom: 0.2em;
   /* color: #000; */
+`
+
+const DateUpdated = styled.p`
+  font-size: 1.9rem;
+  font-weight: 500;
+  margin-top: 0;
 `
 
 function BlogPost({ data }) {
   const heroImage = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <Date>{data.mdx.frontmatter.date}</Date>
+      <Date>posted on {data.mdx.frontmatter.date}</Date>
+      <DateUpdated>
+        {/* show date updated only if it is provided by frontmatter */}
+        {data.mdx.frontmatter.dateUpdated &&
+          `updated: ${data.mdx.frontmatter.dateUpdated}`}{" "}
+      </DateUpdated>
       <GatsbyImage
         image={heroImage}
         alt={data.mdx.frontmatter.hero_image_alt}
@@ -33,6 +45,7 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        dateUpdated(formatString: "MMMM DD, YYYY")
         hero_image_alt
         hero_image {
           childImageSharp {
